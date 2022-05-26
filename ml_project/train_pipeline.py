987a3369a8ea7edba02930911625e77c16e70025
RonWise <7211393+RonWise@users.noreply.github.com>
2022-05-26 18:14:53 +0300

    minor

--- ml_project/train_pipeline.py
+++ ml_project/train_pipeline.py
@@ -63,10 +63,6 @@ def train_pipeline(config: DictConfig):
             # Model registry does not work with file store
             if tracking_url_type_store != "file":
 
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
                 mlflow.sklearn.log_model(model, "model", registered_model_name="Model " + training_pipeline_params.model.model_params.model_type)
             else:
                 mlflow.sklearn.log_model(model, "model")
